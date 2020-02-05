1c3502752ac0062f8106b801c54afd24dc709988
showlee-j <ldnono345@naver.com>
2020-02-05 17:55:38 +0900

    study

--- project_euler/problem10.py
+++ project_euler/problem10.py
@@ -1,29 +1,31 @@
 '''
 이백만 이하 소수의 합
+
+지금까지 구해왔던 소수로는 시간이 너무 오래 걸려
+에라토스테네스의 체를 사용하니 시간이 엄청나게 빠르다.
 '''
 
-sum = 0
+# 에라토스테네스의 체
+def prime(n):
+    # 에라토스테네스의 체로 n개 요소에 True 설정 소수라 생각.
+    primeList = [True] * n
+    answer = []
 
-def Prime(n):
-    if(n<2):
-        return False
+    # n의 최대 약수가 sqrt(n) 이하라서 sqrt(n)까지만 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if primeList[i] == True: # i가 소수인 경우
+            for j in range(i+i, n, i): # i의 배수들을 False 판정
+                primeList[j] = False
+
+    # True가 소수이므로 True index만 다른 리스트에 저장장
     for i in range(2,n):
-        if(n%i==0):
-            return False
-    return True
+        if primeList[i] == True :
+            answer.append(i)
 
-for i in range(2,2000000):
-    if(Prime(i)):
-        sum+=i
-print(sum)
+    return answer
 
-
-
-
-
-
-
-
+print(sum(prime(2000000)))
 
 
 
---
+++ project_euler/problem12.py
@@ -0,0 +1,26 @@
+'''
+500개 이상의 약수를 갖는 가장 작은 삼각수는?
+
+삼각수 : 1부터 n까지의 자연수를 차례로 더하여 구해진 값을 삼각수라고 한다
+Ex) 7번 째 삼각수는 1+2+3+4+5+6+7 = 28
+1,3,10,15,21,28,36,45,55 ...
+
+미완성
+'''
+
+li = list(range(1,100000))
+
+sum=0
+li2 = []
+for i in li :
+    sum+=i
+    for j in range(2,sum):
+        if j>sum / i :
+            break
+        if sum % j== 0:
+            li2.append(j)
+            if sum//j!=j:
+                li2.append(sum//j)
+    if len(li2)>=500:
+        print(sum)
+        break
